8039520bdd48b8015c64981837e861243727226e
urielloera-dot <uriel.loera@gmail.com>
2026-06-10 22:20:46 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "fieldops-v1";
+const ASSETS = ["/", "/index.html"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  // Network first, fall back to cache
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
